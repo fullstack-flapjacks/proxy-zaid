9a2c2f4d7276236f2814e0ee272217d1d0c6ee18
zaid <zaidhusseini@gmail.com>
2018-03-05 15:07:06 -0800

    Implement About microservice on proxy server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use('/r/:id', express.static('public'));
+
+app.listen(PORT, ()=>console.log('Proxy Server listening on PORT', PORT));
+
+
